b223fef661a764c18d5fe0558664a17eddf30c68
Mohamed Atef <atefdodo@gmail.com>
2026-04-12 15:32:45 +0200

    Update Md file

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -1,8 +1,8 @@
 @php
-$containerFooter =
-isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact'
-? 'container-xxl'
-: 'container-fluid';
+  $containerFooter =
+      isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact'
+          ? 'container-xxl'
+          : 'container-fluid';
 @endphp
 
 <!-- Footer-->
@@ -14,13 +14,15 @@ isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact
         <script>
           document.write(new Date().getFullYear());
         </script>
-        , made with ❤️ by <a href="{{ !empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '' }}" target="_blank" class="footer-link">{{ !empty(config('variables.creatorName')) ? config('variables.creatorName') : '' }}</a>
+        , made with ❤️ by Mohamed Atef</a>
       </div>
       <div class="d-none d-lg-inline-block">
-        <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}" class="footer-link me-4" target="_blank">License</a>
-        <a href="{{ config('variables.moreThemes') ? config('variables.moreThemes') : '#' }}" target="_blank" class="footer-link me-4">More Themes</a>
-        <a href="{{ config('variables.documentation') ? config('variables.documentation') . '/laravel-introduction.html' : '#' }}" target="_blank" class="footer-link me-4">Documentation</a>
-        <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank" class="footer-link">Support</a>
+        <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}" class="footer-link me-4"
+          target="_blank">License</a>
+        <a href="{{ config('variables.documentation') ? config('variables.documentation') . '/laravel-introduction.html' : '#' }}"
+          target="_blank" class="footer-link me-4">Documentation</a>
+        <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank"
+          class="footer-link">Support</a>
       </div>
     </div>
   </div>
